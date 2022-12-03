3a59f815197a741dad9f5e87e6131ab4633d3279
trungnhan <lonelytoo2007@gmail.com>
2022-12-03 16:10:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import MyRouter from "routers/index";
-import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
       <MyRouter />
-      <MessengerCustomerChat
-        pageId="100088111072986"
-        appId="5766874846710177"
-      />
     </div>
   );
 }
